9b1afa2beac4de10b6f7d655de39cffbe0663846
Jett Thompson <jett.thompson.59@gmail.com>
2021-04-24 08:57:03 +1000

    added an app.java

---
+++ src/main/java/app/cleancode/chess/App.java
@@ -0,0 +1,16 @@
+package app.cleancode.chess;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+public static void main(String[] args) {
+	launch (args);
+}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.close();
+	}
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
 open module app.cleancode.fiveDChess {
+	requires javafx.graphics;
 	
 }
